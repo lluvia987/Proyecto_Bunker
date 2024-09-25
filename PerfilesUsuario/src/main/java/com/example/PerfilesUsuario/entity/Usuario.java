50b93b9b8035495a00dcdec615c0af61d31e4f3f
cesar <cesarxx_19@hotmail.com>
2024-09-25 10:05:02 -0500

    add entity.usuarios

---
+++ PerfilesUsuario/src/main/java/com/example/PerfilesUsuario/entity/Usuario.java
@@ -0,0 +1,13 @@
+package com.example.PerfilesUsuario.entity;
+
+public class Usuario {
+    /*ID único (Genracion automatica de 6 digitos, UUID)
+    Correo electrónico (obligatorio)
+    Contraseña (hash, obligatorio)
+    Ninck Name (Obligatorio)
+    Foto (Opcional)
+    Fecha de Creación (automatico)
+    */
+
+}
+
