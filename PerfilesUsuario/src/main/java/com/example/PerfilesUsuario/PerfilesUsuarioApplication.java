4ac022fde7529ab8b1bee2c37cd163d9624b6b64
cesar <cesarxx_19@hotmail.com>
2024-09-25 09:37:09 -0500

    add microservicio usuarios

---
+++ PerfilesUsuario/src/main/java/com/example/PerfilesUsuario/PerfilesUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.PerfilesUsuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilesUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilesUsuarioApplication.class, args);
+	}
+
+}
---
+++ PerfilesUsuario/src/test/java/com/example/PerfilesUsuario/PerfilesUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PerfilesUsuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilesUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
